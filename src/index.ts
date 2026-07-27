015d5b78832b7404e127b400909c8b9aa40dead7
just-shadyumbrella <xshadyumbrella@proton.me>
2026-07-27 17:17:02 +0700

    local source

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import fs from "node:fs";
 import path from "node:path";
 import { Readable } from "node:stream";
-import { Hono } from "hono";
+import { Hono, Context } from "hono";
 import { proxy } from "hono/proxy";
-import { decrypt, hash } from "./sniffy.js";
+import { fileTypeFromFile } from "file-type";
 import { db, url } from "./db.js";
+import { decrypt, hash } from "./sniffy.js";
 
 const app = new Hono();
 const PAGE_ROOT = path.resolve(process.cwd(), "page");
@@ -22,7 +23,7 @@ function resolvePagePath(reqPath: string) {
   return path.resolve(PAGE_ROOT, `${safeRelative}.html`);
 }
 
-function streamFile(filePath: string) {
+function streamFile(filePath: string, mime = "application/octet-stream") {
   try {
     const absolutePath = path.resolve(filePath);
 
@@ -37,7 +38,7 @@ function streamFile(filePath: string) {
     return new Response(webStream, {
       headers: {
         "Content-Length": String(stats.size),
-        "Content-Type": "text/html; charset=utf-8",
+        "Content-Type": mime,
       },
     });
   } catch {
@@ -45,52 +46,58 @@ function streamFile(filePath: string) {
   }
 }
 
-app.get("/*", async (c) => {
-  if (c.req.path.startsWith("/favicon")) {
-    return proxy("https://avatars.githubusercontent.com/u/249699734");
+async function cbtHandler(c: Context) {
+  const path = c.req.url.replace(/^https?:\/\/[^/]+/, "");
+  const ip =
+    c.req.header("x-real-ip") ||
+    c.req.header("x-forwarded-for")?.split(",")[0].trim();
+  const ua = c.req.header("user-agent");
+  const sec = `Sec-CH-UA: ${c.req.header("sec-ch-ua")}; Sec-CH-UA-Mobile: ${c.req.header("sec-ch-ua-mobile")}; Sec-CH-UA-Platform: ${c.req.header("sec-ch-ua-platform")}`;
+  let success = false;
+  const id = hash(`${path}:${ip}:${success}:${ua}:${sec}`);
+
+  const param = c.req.query("code") || "";
+  let res = {} as Response;
+  try {
+    res = c.redirect(decrypt(param));
+    success = true;
+  } catch {
+    res = c.redirect("https://files.catbox.moe/ul5orj.mp4");
   }
-  if (c.req.path === "/cbt") {
-    const path = c.req.url.replace(/^https?:\/\/[^/]+/, "");
-    const ip =
-      c.req.header("x-real-ip") ||
-      c.req.header("x-forwarded-for")?.split(",")[0].trim();
-    const ua = c.req.header("user-agent");
-    const sec = `Sec-CH-UA: ${c.req.header("sec-ch-ua")}; Sec-CH-UA-Mobile: ${c.req.header("sec-ch-ua-mobile")}; Sec-CH-UA-Platform: ${c.req.header("sec-ch-ua-platform")}`;
-    let success = false;
-    const id = hash(`${path}:${ip}:${success}:${ua}:${sec}`);
 
-    const param = c.req.query("code") || "";
-    let res = {} as Response;
-    try {
-      res = c.redirect(decrypt(param));
-      success = true;
-    } catch {
-      res = c.redirect("https://files.catbox.moe/ul5orj.mp4");
-    }
-
-    await db
-      .insert(url)
-      .values({
-        id,
-        success,
+  await db
+    .insert(url)
+    .values({
+      id,
+      success,
+      modified: new Date().toISOString(),
+      url: c.req.url.replace(/^https?:\/\/[^/]+/, ""),
+      ip,
+      ua,
+      sec,
+    })
+    .onConflictDoUpdate({
+      target: url.id,
+      set: {
         modified: new Date().toISOString(),
-        url: c.req.url.replace(/^https?:\/\/[^/]+/, ""),
-        ip,
-        ua,
-        sec,
-      })
-      .onConflictDoUpdate({
-        target: url.id,
-        set: {
-          modified: new Date().toISOString(),
-        },
-      });
+      },
+    });
 
-    return res;
-  }
+  return res;
+}
 
-  const pagePath = resolvePagePath(c.req.path);
-  return streamFile(pagePath);
+async function publicHandler(c: Context) {
+  const filePath = path.resolve(process.cwd(), `.${c.req.path}`);
+  if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) return new Response(null, { status: 404 });
+  const fileType = await fileTypeFromFile(filePath);
+  return streamFile(filePath, fileType?.mime);
+}
+
+app.get("/*", async (c) => {
+  if (c.req.path.startsWith("/favicon")) return proxy("https://avatars.githubusercontent.com/u/249699734");
+  if (c.req.path === "/cbt") return cbtHandler(c);
+  if (c.req.path.startsWith("/public")) return publicHandler(c);
+  return streamFile(resolvePagePath(c.req.path), "text/html; charset=utf-8");
 });
 
 app.notFound(() => new Response(null, { status: 404 }));
