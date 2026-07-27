8f188597786cd3bfda84ba5957d544a7486d69b7
just-shadyumbrella <xshadyumbrella@proton.me>
2026-07-28 01:33:26 +0700

    debug2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import fs from "node:fs";
 import path from "node:path";
-import { Readable } from "node:stream";
 import { Hono, Context } from "hono";
 import { proxy } from "hono/proxy";
 import { db, url } from "./db.js";
@@ -11,33 +10,15 @@ const app = new Hono();
 // Trigger bundler to include files
 const PAGE_ROOT = path.resolve(process.cwd(), "page");
 
-/**
- * Please handle with `try...catch`
- */
-function streamFile(filePath: string, mime = "application/octet-stream") {
-  console.log(filePath);
-  console.log(process.cwd());
-  console.log(fs.readdirSync(process.cwd()));
-  console.log(fs.readdirSync(path.dirname(filePath)));
-  const stream = fs.createReadStream(filePath);
-  const streamWeb = Readable.toWeb(stream) as unknown as ReadableStream;
-  return new Response(streamWeb, {
-    headers: {
-      "Content-Length": String(fs.statSync(filePath).size),
-      "Content-Type": mime,
-    },
-  });
-}
-
 async function cbtHandler(c: Context) {
-  const path = c.req.url.replace(/^https?:\/\/[^/]+/, "");
+  const reqPath = c.req.url.replace(/^https?:\/\/[^/]+/, "");
   const ip =
     c.req.header("x-real-ip") ||
     c.req.header("x-forwarded-for")?.split(",")[0].trim();
   const ua = c.req.header("user-agent");
   const sec = `Sec-CH-UA: ${c.req.header("sec-ch-ua")}; Sec-CH-UA-Mobile: ${c.req.header("sec-ch-ua-mobile")}; Sec-CH-UA-Platform: ${c.req.header("sec-ch-ua-platform")}`;
   let success = false;
-  const id = hash(`${path}:${ip}:${success}:${ua}:${sec}`);
+  const id = hash(`${reqPath}:${ip}:${success}:${ua}:${sec}`);
 
   const param = c.req.query("code") || "";
   let res = {} as Response;
@@ -81,13 +62,15 @@ app.get("/*", async (c, next) => {
   if (c.req.path.startsWith("/favicon"))
     return proxy("https://avatars.githubusercontent.com/u/249699734");
   if (c.req.path === "/cbt") return cbtHandler(c);
-  return streamFile(
+  const file = fs.readFileSync(
     path.join(
       PAGE_ROOT,
       `.${c.req.path.endsWith("/") ? `${c.req.path}index` : c.req.path}.html`,
     ),
-    "text/html; charset=utf-8",
   );
+  return new Response(file, {
+    headers: { "Content-Type": "text/html; charset=utf-8" },
+  });
 });
 
 export default app;
